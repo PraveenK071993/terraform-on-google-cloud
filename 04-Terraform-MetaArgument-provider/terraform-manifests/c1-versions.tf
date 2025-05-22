ebeb0057f7c3a8ad6ba23c0dd693c937e1ba1dad
PraveenK071993 <praveen.k071993@gmail.com>
2025-05-22 13:45:42 +0530

    Practiced

--- 04-Terraform-MetaArgument-provider/terraform-manifests/c1-versions.tf
+++ 04-Terraform-MetaArgument-provider/terraform-manifests/c1-versions.tf
@@ -1,24 +1,25 @@
 # Terraform Settings Block
 terraform {
-  required_version = ">= 1.8"
+  required_version = "~> 1.12.0"
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 5.34.0"
+      version = "~> 6.36.0"
     }
   }
 }
 
-# Terraform Provider-1: us-central1
+
+# Terraform Provider-1: provider1
 provider "google" {
-  project = "gcplearn9"
+  project = "silver-fiber-457005-b8" #PROJECT_ID
   region = "us-central1"
-  alias = "us-central1"    
+  alias = "provider1"
 }
 
-# Terraform Provider-2: europe-west1
-provider "google" {
-  project = "gcplearn9"
+# Terraform Provider-2: provider2
+rovider "google" {
+  project = "silver-fiber-457005-b8" #PROJECT_ID
   region = "europe-west1"
-  alias = "europe-west1"    
+  alias = "provider2"
 }
