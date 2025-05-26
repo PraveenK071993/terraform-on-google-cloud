ea014ce6cacf067761dd1bf334ae0d46f1a66d4b
PraveenK071993 <praveen.k071993@gmail.com>
2025-05-26 13:53:53 +0530

    new

--- 06-Terraform-MetaArgument-count/terraform-manifests/c6-output-values.tf
+++ 06-Terraform-MetaArgument-count/terraform-manifests/c6-output-values.tf
@@ -9,57 +9,57 @@
 
 # Get each list item separately
 output "vm_name_0" {
-  description = "VM Name"
+  description = "VM NAME"
   value = google_compute_instance.myapp1[0].name
+  
 }
 
-# Get each list item separately
 output "vm_name_1" {
-  description = "VM Name"
+  description = "VM NAME"
   value = google_compute_instance.myapp1[1].name
+  
 }
 
-# Output - For Loop with List
+#Output - For Loop with List
 output "for_output_list" {
   description = "For Loop with List"
   value = [for instance in google_compute_instance.myapp1: instance.name]
+  
 }
 
-# Output - For Loop with Map
+#Output - For Loop with Map
 output "for_output_map1" {
   description = "For Loop with Map"
   value = {for instance in google_compute_instance.myapp1: instance.name => instance.instance_id}
+  
 }
 
-# Output - For Loop with Map Advanced
+#Output - For Loop with Map Advanced
 output "for_output_map2" {
   description = "For Loop with Map - Advanced"
-  value = {for c, instance in google_compute_instance.myapp1: c => instance.name}
+  value = {for c, instance in google_compute_instance.myapp1: c => instance.instance_id}
+  
 }
 
-# Output - For Loop with Map Advanced
+#Output - For Loop with Map Advanced
 output "for_output_map3" {
-  description = "For Loop with Map - Advanced (Instance Name and Instance ID)"
-  value = {for c, instance in google_compute_instance.myapp1: instance.name => instance.instance_id}
-}
-
-# VM External IPs
-output "vm_external_ips" {
   description = "For Loop with Map - Advanced"
-  value = {for c, instance in google_compute_instance.myapp1: c => instance.network_interface.0.access_config.0.nat_ip}
+  value = {for c, instance in google_compute_instance.myapp1: instance.name => instance.instance_id}
+  
 }
 
-
-# Output Legacy Splat Operator (Legacy) - Returns the List
-output "legacy_splat_instance" {
+#Legacy Splat Operator (latest) - Returns List
+output "legacy_splat_operator" {
   description = "Legacy Splat Operator"
   value = google_compute_instance.myapp1.*.name
+  
 }
 
-# Output Latest Generalized Splat Operator - Returns the List
-output "latest_splat_instance" {
-  description = "Generalized latest Splat Operator"
-  value = google_compute_instance.myapp1[*].name 
+#Latest Generalized Splat Operator - Returns the List
+output "legacy_splat_instance" {
+  description = "Generalized Latest Splat Operator"
+  value = google_compute_instance.myapp1[*].name
+  
 }
 
 /* 
